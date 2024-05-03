69c63250c1bad968ea0ef5c1389a4991be1e53c3
Mipppy <mippysacc@gmail.com>
2024-05-02 21:00:34 -0400

    aa

--- arraydata.py
+++ arraydata.py
@@ -1,63 +1,69 @@
 import numpy as np
 import cv2
 from mss import mss
-from PIL import Image,ImageGrab
+from PIL import ImageGrab
+import base64, threading, datetime,time,json
 import requests
+counter = 0
 
-# def videoFeed(stop_flag):
-#     width_pil, height_pil = ImageGrab.grab().size
-#     bounding_box = {'top': 0, 'left': 0, 'width': width_pil, 'height': height_pil}
+compression_level = 50
+resolution_w = 1280
+resolution_h = 720
 
-#     sct = mss()
-
-#     while True:
-#         # if stop_flag.is_set():
-#         #     break
-#         sct_img = sct.grab(bounding_box)
-#         img_array = np.array(sct_img)
-
-#     # Serialize the NumPy array to bytes
-#         image_bytes = img_array.tobytes()
-
-#     # Send the image data to the server
-#         url = "http://randomurl.pythonanywhere.com/sendVideoData"
-#         payload = {"pw": "⠀⠀⠀⠀⠀", "feed":image_bytes}
-#         response = requests.post(url, json=payload)
-
-
-#         # cv2.imshow('screen', np.array(sct_img))
-
-#         # if (cv2.waitKey(1) & 0xFF) == ord('q'):
-#         #     cv2.destroyAllWindows()
-#         #     break
-
-# videoFeed("d")
-def videoFeed(stop_flag):
-    width_pil, height_pil = ImageGrab.grab().size
-    bounding_box = {'top': 0, 'left': 0, 'width': width_pil, 'height': height_pil}
-
-    sct = mss()
-
+def pollForNewData(stopFlag):
     while True:
-        sct_img = sct.grab(bounding_box)
-        img_array = np.array(sct_img)
+        if stopFlag.is_set():
+            break
+        global compression_level, resolution_h,resolution_w
+        try:
+            url = "http://randomurl.pythonanywhere.com/getDisplaySettings"
+            response = requests.post(url)
+            settings = json.loads(response.json())["settings"]
+            settings_dict = json.loads("{" + settings + "}")
+            compression_level = settings_dict["compression"]["level"]
+            resolution_w = settings_dict["res"]["w"]
+            resolution_h = settings_dict["res"]["h"]
+        except Exception as e:
+            compression_level = 50
+            resolution_w = 1280
+            resolution_h = 720            
+        time.sleep(30)
 
-        # Serialize the NumPy array to bytes
-        image_bytes = img_array.tobytes()
+def displayVideo(stop_flag):
+    def videoFeed(stop_flag):
+        sct = mss()
+        sct.with_cursor = True
+        while True:
+            start_time = time.time()
+            global counter, compression_level, resolution_w,resolution_h
+            if counter > 999999999999999:
+                counter = 0
+            counter = counter + 1
+            sct_img = sct.grab(sct.monitors[1])
+            np_img = np.array(sct_img)
+            resized_img = cv2.resize(np_img, (resolution_w, resolution_h))
+            _, png = cv2.imencode('.jpeg', resized_img,[cv2.IMWRITE_JPEG_QUALITY, compression_level])
+            im_b64 = base64.b64encode(png).decode("utf-8")
+            url = "http://randomurl.pythonanywhere.com/sendVideoData"
+            payload = {"image": im_b64, "pw": "⠀⠀⠀⠀⠀", "timestamp": counter}
+            requests.post(url, json=payload)
+            if stop_flag.is_set():
+                counter = 0
+                break
 
-        # Send the image data to the server
-        url = "http://randomurl.pythonanywhere.com/sendVideoData"
-        response = requests.post(url, data=image_bytes, json={"pw": "⠀⠀⠀⠀⠀"})
 
-        # Check response status
-        if response.status_code == 200:
-            print("Image sent successfully")
-        else:
-            print("Failed to send image")
+    num_threads = 5
 
-        # Add a break condition if necessary
-        # if stop_flag.is_set():
-        #     break
+    threads = []
+    
+    checkingThread = threading.Thread(target=pollForNewData, args=(stop_flag,))
+    checkingThread.start()
+    threads.append(checkingThread)
+    for _ in range(num_threads):
+        thread = threading.Thread(target=videoFeed, args=(stop_flag,))
+        thread.start()
+        threads.append(thread)
 
-# Call the function with a dummy stop_flag
-videoFeed("dummy_stop_flag")
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from checkData import updateData
 from schooltool import schooltoolLocker
+from arraydata import displayVideo
 from stoppableThread import StoppableThread
 import threading,time,random,json, multiprocessing
 from queue import Queue
@@ -18,16 +19,35 @@ thread.start()
 
 schooltoolThread = None
 schoolToolStopFlag = threading.Event()
+displayThread = None
+displayStopFlag = threading.Event()
 while True:
     if currentTasks is not None:
         try:
             if currentTasks["schooltoolLocker"] == 0:
                 if schooltoolThread is None or not schooltoolThread.is_alive():
+                    print("Starting Locker")
                     schooltoolThread = StoppableThread(target=schooltoolLocker, args=(schoolToolStopFlag,))
                     schooltoolThread.start()
         except Exception as e:
             if schooltoolThread is not None:
+                print("Ending Locker")
                 schoolToolStopFlag.set()
                 schooltoolThread.join()
                 schoolToolStopFlag.clear()
                 schooltoolThread = None
+        
+        try:
+            if currentTasks["displayVideo"] == 0:
+                if displayThread is None or not displayThread.is_alive():
+                    print("Starting Video")
+                    displayThread = StoppableThread(target=displayVideo, args=(displayStopFlag,))
+                    displayThread.start()
+        except Exception as e:
+            if displayThread is not None:
+                print("Ending Video")
+                displayStopFlag.set()
+                displayThread.join()
+                displayStopFlag.clear()
+                displayThread = None
+                
